74f0b899423a98eeed7b6400d227a4cd73f6ee77
ZhuIsaac <chongisaac06@gmail.com>
2024-07-13 13:11:01 +0800

    Update Pokemon.java
    Added my code - Isaac

--- Pokemon Ga Ole/src/Pokemon.java
+++ Pokemon Ga Ole/src/Pokemon.java
@@ -1,4 +1,62 @@
 
 public class Pokemon {
+	//Attributes
+	private String name;
+	private String element;
+//private int star; (Maybe, maybe not)
+	private int hp;
+	private int baseAtk;
+	private int baseDef;
+	public final static float SUPER_EFFECTIVE = 2f;
+	public final static float NOT_VERY_EFFECTIVE = 1.5f;
+	public final static float NORMAL = 1f;
+	
+	
+	//Constructor
+	public Pokemon(String name, String element, int hp, int baseAtk, int baseDef) {
+		this.name = name;
+		this.element = element;
+		this.hp = hp;
+		this.baseAtk = baseAtk;
+		this.baseDef = baseDef;
+	}
 
+	//Setter & getters
+	public String getName() {
+		return name;
+	}
+
+	public String getElement() {
+		return element;
+	}
+
+//	public int getStar() {
+//		return star;
+//	}
+	
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getBaseAtk() {
+		return baseAtk;
+	}
+
+
+	public int getBaseDef() {
+		return baseDef;
+	}
+  
+	//Other methods
+	
+	
+	//toString
+	@Override
+	public String toString() {
+		return String.format("Pokemon [name=%s, element=%s, hp=%s, baseAtk=%s, baseDef=%s]", name, element, hp, baseAtk,
+				baseDef);
+	}
 }
