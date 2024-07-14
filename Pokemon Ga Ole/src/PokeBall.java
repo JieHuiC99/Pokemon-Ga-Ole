4b8b5a87a827a414137e12c9a9d7065c5849c25b
yhui04 <22010425@imail.sunway.edu.my>
2024-07-14 15:57:17 +0800

    Update PokeBall.java

--- Pokemon Ga Ole/src/PokeBall.java
+++ Pokemon Ga Ole/src/PokeBall.java
@@ -1,15 +1,22 @@
+public class PokeBall {
+    public final static int POKE = 2;
+    public final static int GREAT = 4;
+    public final static int ULTRA = 6;
+    
+    private String type;
+    private int chance;
+    private int numOfChance;
 
-public abstract class PokeBall {
-	
-	public final static int POKE   = 2;
-	public final static int GREAT  = 4;
-	public final static int ULTRA  = 6;
-	private String type;
-	private int chance;
-	
-	public PokeBall(int chance, String type) {
-		this.chance = chance;
-		this.type = type;
-	}
-	
+    public PokeBall(int chance, String type) {
+        this.chance = chance;
+        this.type = type;
+    }
+
+    public int getChance() {
+        return chance;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
