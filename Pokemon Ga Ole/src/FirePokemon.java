929d8820263a25726ac4c178c79500c16fd41378
ChanXianKang <18016386@imail.sunway.edu.my>
2024-07-16 13:14:34 +0800

    FirePokemon.java

--- Pokemon Ga Ole/src/FirePokemon.java
+++ Pokemon Ga Ole/src/FirePokemon.java
@@ -1,7 +1,7 @@
 
 public class FirePokemon extends Pokemon {
 
-	public int damage, atkVal;
+	public int damage, keyVal;
 	public static float rate; 
 	private Pokemon enemyP;
 	
@@ -14,11 +14,11 @@ public class FirePokemon extends Pokemon {
 	}
 
 	//Override setEnemyP for FirePokemon
-	public void setEnemyP(Pokemon enemyP) {
-		super.setEnemyP(enemyP);
-		this.enemyP = enemyP;
-		updateRate();
-	}
+//	public void setEnemyP(Pokemon enemyP) {
+//		super.setEnemyP(enemyP);
+//		this.enemyP = enemyP;
+//		updateRate();
+//	}
 	
 	
 	//Element effectiveness
@@ -34,29 +34,31 @@ public class FirePokemon extends Pokemon {
 	}}
 	
 	//Calculate attack damage
-//	public int getDamage(int atkVal, float rate) {
+//	public int getDamage(int keyVal, float rate) {
 //		int atkDmg;
-//		atkDmg = (int)(((atkVal + getBaseAtk()) * rate) - getEnemyP().getBaseDef());
+//		atkDmg = (int)(((keyVal + getBaseAtk()) * rate) - getEnemyP().getBaseDef());
 //		return atkDmg;
 //	}
 	
-	public int getDamage(int atkVal, float rate, Pokemon enemyP) {
+	public int getDamage(int keyVal, float rate, Pokemon enemyP) {
 		int atkDmg;
-		atkDmg = (int)(((atkVal + getBaseAtk()) * rate) - enemyP.getBaseDef());
+		atkDmg = (int)(((keyVal + getBaseAtk()) * rate) - enemyP.getBaseDef());
 		return atkDmg;
 	}
 	
-	//Calculate enemy's HP after attack 
-	public void AttackVal(int atkVal, Pokemon enemyP) {
+	   /* Calculate the damage, and then subtract the enemy's hp based on the value
+	    * @param keyVal      - the number of times player spammed the key (GUI feature)
+	    * @param enemyP      - enemy pokemon object */ 
+	public void attackVal(int keyVal, Pokemon enemyP) {
 		if (enemyP instanceof FirePokemon) {			
-		    enemyP.setHp(enemyP.getHp() - getDamage(atkVal, SUPER_EFFECTIVE, enemyP));
+		    enemyP.setHp(enemyP.getHp() - getDamage(keyVal, SUPER_EFFECTIVE, enemyP));
 		}
 		else if (enemyP instanceof ElectricPokemon){
-			enemyP.setHp(enemyP.getHp() - getDamage(atkVal, NORMAL, enemyP));
+			enemyP.setHp(enemyP.getHp() - getDamage(keyVal, NORMAL, enemyP));
 				
 		}
 		else if (enemyP instanceof WaterPokemon){
-			enemyP.setHp(enemyP.getHp() - getDamage(atkVal, NOT_VERY_EFFECTIVE, enemyP));					
+			enemyP.setHp(enemyP.getHp() - getDamage(keyVal, NOT_VERY_EFFECTIVE, enemyP));					
 		}
 	}
 	
