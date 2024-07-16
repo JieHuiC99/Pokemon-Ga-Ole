7a37eaaa8e537e7da1d453087ed747221eeb66ce
ChanXianKang <18016386@imail.sunway.edu.my>
2024-07-16 13:15:49 +0800

    PokeGame.java

--- Pokemon Ga Ole/src/PokeBack.java
+++ Pokemon Ga Ole/src/PokeBack.java
@@ -1,19 +1,22 @@
-import java.util.ArrayList;
-public class PokeBack {
+import java.util.*;
+public class PokeGame {
 	
 //	public final static int LOCATION_FOREST          = 1;
 //	public final static int VOLCANO                  = 2;
 //	public final static int ICEFALL_CAVE             = 3;
 //	public final static int WATER_LABYRINTH          = 4;
-	Pokemon LocationP1, LocationP2, LocationP3;
+//	Pokemon LocationP1, LocationP2, LocationP3;
+	
 	
 	Pokemon[][] grid;
-	ArrayList<Pokemon> Location = new ArrayList<Pokemon>();
-	
+	ArrayList<Pokemon> Ocean = new ArrayList<Pokemon>();
+	ArrayList<Pokemon> Volcano = new ArrayList<Pokemon>();
+	ArrayList<Pokemon> Forest = new ArrayList<Pokemon>();
+		
 	Account player = new Account(0);
 	Account npc = new Account(1);
 	Displayer d = new Displayer();
-	
+	Scanner input = new Scanner(System.in);
 	
 	
 //	private void GenerateGrid() {
@@ -39,14 +42,80 @@ public class PokeBack {
 	
 	
 	
-	public PokeBack() {
+	public PokeGame() {
 		grid = new Pokemon[3][3];
 //		Location.add(LocationP1);
 //		Location.add(LocationP2);
 //		Location.add(LocationP3);
+		//Pokemon pikachu = new ElectricPokemon("Pikachu", "ELECTRIC", 1000,1000,500);
+		
+		
 	}
 	
+	public void start() {
+		
+		
+		
+		Account a1 = new Account(0);
+		Account a2 = new Account(1);
+
+		
+		boolean running = true;
+		
+		
+		while(running) {
+			try {				
+				
+				System.out.println("1. Login");
+				System.out.println("2. Exit");
+				System.out.print("Option: ");
+				int op = input.nextInt();
+				
+				if(op == 1) {
+					
+					//Login---------------------------------------------------------					
+					System.out.print("Enter PlayerID: ");
+					int playerID = input.nextInt();	
+					input.nextLine();
+					System.out.print("Enter Password: ");					
+					String playerPass = input.nextLine();
+					
+					if(a1.IsLoginValid(playerID, playerPass) == true) {  //successfully logged in --------------------
+						ArrayList<Pokemon> pInventory = a1.GetDisk();
+						System.out.println("Succesfully Logged In!");
+						System.out.println("Here is your inventory!");
+						System.out.println(pInventory.size());
+						
+						for( int i = 0; i < pInventory.size(); i++ ) {
+							d.PrintDisk(pInventory.get(i));
+						}
+						
+						System.out.println("Location: Volcano");
+						
+						//d.PrintDisk(p1);
+						
+					}else {  //log in invalid -------------------------------------------------
+						System.out.println("Your ID or Password is invalid!");
+					}
+					
+				}
+			}catch(Exception e) {
+				if(e instanceof InputMismatchException	) {
+					System.out.println("ERROR!: Invalid Input");	
+					input.next();
+				}else {
+					//System.out.println("ERROR!: An error occured!");
+					System.out.println(e.getMessage());
+				}
+				
+			}
+		}
+		
+	}  //===================================================================================================================
 	
+	public boolean login() {
+		return false; //dummy value
+	}
 	
 	public void GeneratePokemon(int op) {
 		
@@ -59,7 +128,7 @@ public class PokeBack {
 	public int Attack(Pokemon p1, Account p2) {
 		ArrayList<Pokemon> inventory = p2.GetDisk();
 		for( int i = 0; i < inventory.size(); i++) {
-			p1.AttackVal(100,inventory.get(i));
+			p1.attackVal(100,inventory.get(i));
 		}
 		return 0;
 	}
@@ -69,6 +138,11 @@ public class PokeBack {
 		for( int i = 0; i < inventory.size(); i++) {
 			d.PrintDisk(inventory.get(i));
 		}
+	}
+	
+	
+	public void SwtichPlayer() {
+		
 	}
 	
 }
