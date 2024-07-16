840725753bc6d1f86e1b00482865ba208aaeb30f
ChanXianKang <18016386@imail.sunway.edu.my>
2024-07-16 10:25:38 +0800

    PokeBack.java

--- Pokemon Ga Ole/src/PokeBack.java
+++ Pokemon Ga Ole/src/PokeBack.java
@@ -10,39 +10,43 @@ public class PokeBack {
 	Pokemon[][] grid;
 	ArrayList<Pokemon> Location = new ArrayList<Pokemon>();
 	
-	private void GenerateGrid() {
-		int randomRow, randomCol;
-		
-		for( int i = 0; i < 1; i++) {
-			do {
-				randomRow = (int)(Math.random() * grid.length);
-	            randomCol = (int)(Math.random() * grid[0].length);
-			}while (grid[randomRow][randomCol] == new Charmander(1, 1000, 100, "FIRE", 1, 3));
-		}
-	}
+	Account player = new Account(0);
+	Account npc = new Account(1);
+	Displayer d = new Displayer();
 	
 	
-	private Pokemon GenerateForestPokemon() {
-		Pokemon[] p = new Pokemon[3];
-		int rand = (int)(Math.random() * 3);
-//		p[0] = new Charmander(1, 1000, 100, "FIRE", 1, 3);
-//		p[1] = new Charmander(1, 1000, 100, "FIRE", 1, 3);
-//		p[2] = new Charmander(1, 1000, 100, "FIRE", 1, 3);
-		return p[rand];
-	}
+	
+//	private void GenerateGrid() {
+//		int randomRow, randomCol;
+//		
+//		for( int i = 0; i < 1; i++) {
+//			do {
+//				randomRow = (int)(Math.random() * grid.length);
+//	            randomCol = (int)(Math.random() * grid[0].length);
+//			}while (grid[randomRow][randomCol] == new Charmander(1, 1000, 100, "FIRE", 1, 3));
+//		}
+//	}
+	
+	
+//	private Pokemon GenerateFirePokemon() {
+//		Pokemon[] p = new Pokemon[3];
+//		int rand = (int)(Math.random() * 3);
+//		p[0] = new FirePokemon("Charmander 1","FIRE",1000,1000,500);
+//		p[1] = new FirePokemon("Charmander 2","FIRE",1000,1000,500);
+//		p[2] = new FirePokemon("Charmander 3","FIRE",1000,1000,500);
+//		return p[rand];
+//	}
 	
 	
 	
 	public PokeBack() {
 		grid = new Pokemon[3][3];
-		Location.add(LocationP1);
-		Location.add(LocationP2);
-		Location.add(LocationP3);
+//		Location.add(LocationP1);
+//		Location.add(LocationP2);
+//		Location.add(LocationP3);
 	}
 	
-	public void Volcano() {
-		Location.set(0, new Charmander(0,0,0,0));
-	}
+	
 	
 	public void GeneratePokemon(int op) {
 		
@@ -50,6 +54,21 @@ public class PokeBack {
 	
 	public Pokemon[][] GetGrid(){
 		return this.grid;
+	}
+	
+	public int Attack(Pokemon p1, Account p2) {
+		ArrayList<Pokemon> inventory = p2.GetDisk();
+		for( int i = 0; i < inventory.size(); i++) {
+			p1.AttackVal(100,inventory.get(i));
+		}
+		return 0;
+	}
+	
+	public void DisplayAllPlayerDisk(Account a) {
+		ArrayList<Pokemon> inventory = a.GetDisk();
+		for( int i = 0; i < inventory.size(); i++) {
+			d.PrintDisk(inventory.get(i));
+		}
 	}
 	
 }
